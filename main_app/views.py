393aaf5f1595751661d107ad24bfe073cfcde813
bsong1124 <bsong1124@gmail.com>
2024-01-09 15:56:22 -0500

    finish lab part1

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
-wonders = []
+wonders = [
+    {'name': 'Great Wall of China', 'country': 'China'},
+    {'name': 'Chichen Itza', 'country': 'Mexico'},
+    {'name': 'Petra', 'country': 'Jordan'},
+    {'name': 'Machu Picchu', 'country': 'Peru'},
+    {'name': 'Christ the Redeemer', 'country': 'Brazil'},
+    {'name': 'Taj Mahal', 'country': 'India'},
+    {'name': 'The Colosseum', 'country': 'Italy'},  
+]
 # Create your views here.
-def home():
-    return render('home.html')
+def home(request):
+    return render(request, 'home.html')
 
 def about(request):
-    return render('about.html')
+    return render(request, 'about.html')
 
 def wonders_index(request):
-    return render('wonders/index.html',{
+    return render(request, 'wonders/index.html',{
         'wonders': wonders
     })
