12dfca26847124d0ccede1e8437355a97563eea7
bsong1124 <bsong1124@gmail.com>
2024-01-09 15:11:08 -0500

    add url routing and views functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+wonders = []
 # Create your views here.
+def home():
+    return render('home.html')
+
+def about(request):
+    return render('about.html')
+
+def wonders_index(request):
+    return render('wonders/index.html',{
+        'wonders': wonders
+    })
