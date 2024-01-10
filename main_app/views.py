ae9139e557efb31a7563754ea7ed2177ca847e72
bsong1124 <bsong1124@gmail.com>
2024-01-10 11:32:58 -0500

    add model class

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
-wonders = [
-    {'name': 'Great Wall of China', 'country': 'China'},
-    {'name': 'Chichen Itza', 'country': 'Mexico'},
-    {'name': 'Petra', 'country': 'Jordan'},
-    {'name': 'Machu Picchu', 'country': 'Peru'},
-    {'name': 'Christ the Redeemer', 'country': 'Brazil'},
-    {'name': 'Taj Mahal', 'country': 'India'},
-    {'name': 'The Colosseum', 'country': 'Italy'},  
-]
+from .models import Wonder
+
+# wonders = [
+#     {'name': 'Great Wall of China', 'country': 'China'}, '700 BC'
+#     {'name': 'Chichen Itza', 'country': 'Mexico'}, 'AD 600'
+#     {'name': 'Petra', 'country': 'Jordan'}, "312 BC"
+#     {'name': 'Machu Picchu', 'country': 'Peru'}, 'AD 1450'
+#     {'name': 'Christ the Redeemer', 'country': 'Brazil'}, 'AD 1931'
+#     {'name': 'Taj Mahal', 'country': 'India'}, 'AD 1643'
+#     {'name': 'The Colosseum', 'country': 'Italy'},  'AD 80'
+#]
+w = Wonder(name='Chicken Itza', country= 'Mexico', year_built='AD 600')
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -17,6 +20,7 @@ def about(request):
     return render(request, 'about.html')
 
 def wonders_index(request):
+    wonders = Wonder.objects.all()
     return render(request, 'wonders/index.html',{
         'wonders': wonders
     })
