82a817109ea04be2266095729d57bda0402d0dd2
bsong1124 <bsong1124@gmail.com>
2024-01-10 12:08:23 -0500

    fix brackets in main_app/urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('wonders/', views.wonders_index, name='index'),
-    path('wonders/<int:wonder_id', views.wonders_details, name='detail')
-}
+    path('wonders/<int:wonder_id>/', views.wonders_details, name='detail')
+]
